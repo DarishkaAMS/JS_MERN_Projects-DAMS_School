9ab04874630f19643df7fd013abb84ab71f04d16
Daryna Nikolaienko <53945387+DarishkaAMS@users.noreply.github.com>
2021-05-31 15:56:34 +0300

    Update createStudent.js
    Update style @ createStudent.js

--- client/components/createStudent/createStudent.js
+++ client/components/createStudent/createStudent.js
@@ -16,7 +16,7 @@ export default function BasicTextFields() {
 
   return (
     <form className={classes.root} noValidate autoComplete="off">
-      <TextField id="standard-basic" label="Standard" />
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
     </form>
   );
 }

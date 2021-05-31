368e265cff8ddb1dd6355e882da006f74541746a
Daryna Nikolaienko <53945387+DarishkaAMS@users.noreply.github.com>
2021-05-31 14:33:49 +0300

    Update App.js
    Build structure @  client/src/App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,35 @@
 import logo from './logo.svg';
+import { Container, AppBar Typography, Grow, Grid } from 'material-ui/core';
+
+import Student from './components/showStudent/showStudent.js';
+import CreateStudent from './components/CreateStudent/CreateStudent.js';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1> DarishkaAMS SkillsBuild Training </h1>
-      </header>
+      <Container maxWidth="lg"> 
+        <AppBar className={classes.appBar} position="static" color="inherit"> 
+          <Typography className={classes.heading} variant="h2" align="center"> DarishkaAMS Students | Create & Show </Typography> 
+        </AppBar>
+        <Grow in>
+          <Container>
+            <Grid container justify="space-between" alignItems="stretch"> 
+              <Grid item xs={12} sm={7}>
+                <AppBar className={classes.appBar} position="static" color="inherit"> 
+                  <Student />
+                </AppBar>
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                <AppBar className={classes.appBar} position="static" color="inherit"> 
+                  <CreateStudent />
+                </AppBar>
+              </Grid>
+            </Grid>
+          </Container>
+        </Grow>
+      </Container>
     </div>
   );
 }

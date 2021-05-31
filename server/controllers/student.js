f59fbda8bfc434fd24899df45c4d226288f72591
Daryna Nikolaienko <53945387+DarishkaAMS@users.noreply.github.com>
2021-05-31 13:43:49 +0300

    Update student.js
    Add createStudent controller

--- server/controllers/student.js
+++ server/controllers/student.js
@@ -8,6 +8,15 @@ export const getStudents = async (req, res) => {
     }
 };
 
-export const createStudent = (req, res) => {
-    res.send('Router is working hard!');
+export const createStudent = async (req, res) => {
+    const student = req.body;
+    
+    const newStudent = new student(student);
+    try {
+        await newStudent.save();
+        
+        res.status(201).json(newStudent);
+    } catch (errors) {
+        res.status(409).json({ message: error.message });
+    }
 };

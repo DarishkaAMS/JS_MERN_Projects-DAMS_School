0b6e964e844e0ef801e0d162720210cf8deffcef
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-05-31 06:21:46 +0300

    Connect MongoDB

--- server/index.js
+++ server/index.js
@@ -9,3 +9,14 @@ app.use(bodyParser.json({limit: "20mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "20mb", extended: true}));
 
 app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://DarishkaAMS:F1_Junemoon_2013@cluster0.dwg55.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, {
+    useNewUrlParser: true, useUnifiedTopology: true
+}).then(() => app.listen(PORT, () => console.log(`Connection is established and running on port: ${PORT}`)
+)).catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);

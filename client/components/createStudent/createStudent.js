9262fd012cf8fd1971c0a15b34597425d2073404
Daryna Nikolaienko <53945387+DarishkaAMS@users.noreply.github.com>
2021-05-31 16:39:49 +0300

    Update createStudent.js
    Add fields and btn @ createStudent.js

--- client/components/createStudent/createStudent.js
+++ client/components/createStudent/createStudent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -19,6 +20,11 @@ export default function BasicTextFields() {
     <h2> Add New Student </h2>
     <form className={classes.root} noValidate autoComplete="off">
       <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      <Button variant="contained" color="primary">
+        Add Student
+      </Button>
     </form>
     </>
   );
